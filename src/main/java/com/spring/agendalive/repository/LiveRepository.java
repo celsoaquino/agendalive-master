1e0ae8a857527bed04312448f1847e5d48f93e4e
Celso Aquino <celso.aquino@ymail.com>
2020-06-17 23:18:13 -0300

    first commit

---
+++ src/main/java/com/spring/agendalive/repository/LiveRepository.java
@@ -0,0 +1,17 @@
+package com.spring.agendalive.repository;
+
+import com.spring.agendalive.document.LiveDocument;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+
+
+@Repository
+public interface LiveRepository extends MongoRepository<LiveDocument, String> {
+
+    Page<LiveDocument> findByLiveDateAfterOrderByLiveDateAsc(LocalDateTime date, Pageable pageable);
+    Page<LiveDocument> findByLiveDateBeforeOrderByLiveDateDesc(LocalDateTime date, Pageable pageable);
+}
